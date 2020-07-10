dfc817131b389696ea23380a0187d3d1768cf769
CleverParty <shanatmail@gmail.com>
2020-07-10 12:35:08 -0400

    [update] changes to Django flow

--- interface/holder/views.py
+++ interface/holder/views.py
@@ -3,9 +3,16 @@ from sympy import *
 
 def index(request):
     x = Symbol('x')
-    y = Symbol('y')
     f = 5*x**3+12
-    fa = 6*x*y + x**2*y + 26 
     d = f.diff(x)
-    html = "<html><body>The Partial Derivate of 6*x*y + x**2*y + 26 is := %s</body></html>" % diff(fa,x)
+    html = "<html><body>The Derivate of  5*x**3+12 := %s </body></html>" % d
     return HttpResponse(html)
+
+def partial(request):
+    x = Symbol('x')
+    y = Symbol('y')
+    fa = 6*x*y + x**2*y + 26
+    html = "<html><body>The Partial Derivate of 6*x*y + x**2*y + 26 is := %s </body></html>" % diff(fa,x)
+    return HttpResponse(html)
+
+
