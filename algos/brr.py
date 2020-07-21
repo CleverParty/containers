5f09ef54ef08c23ef04219e315360ca8666c88a7
CleverParty <shanatmail@gmail.com>
2020-07-21 15:31:36 -0400

    [update] changes

---
+++ algos/brr.py
@@ -0,0 +1,31 @@
+class Tree:
+    def __init__(self, cargo , left=None , right=None , root=None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+        self.root = root
+
+    def __str__(self):
+        return 'this is a node with the cargo {self.cargo}, in a {self.root} tree'.format(self=self)
+def Total(tree):
+    if tree == None :
+        return 0
+    else :
+        return Total(tree.left) + Total(tree.right) + tree.cargo
+
+
+
+
+
+left = Tree(1)
+right = Tree(3)
+root = Tree(2)
+ultraRoot = Tree(1)
+ultraRoot.right = root
+root.right = right
+root.left = left
+
+root.root = ultraRoot
+
+
+print(Total(ultraRoot))
