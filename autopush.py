4fe3563aa6f877eeda221c8a8d12619dce1b8b32
CleverParty <shanatmail@gmail.com>
2020-07-22 18:07:37 -0400

    [update] auto 2.2

--- autopush.py
+++ autopush.py
@@ -1,13 +1,17 @@
 import subprocess
 import time
 s = subprocess.getstatusoutput(f'git status')
+print("\n)")
+print(s)
 if (s[0] == 0):
     one = subprocess.getstatusoutput(f'git add .')
     time.sleep(20)
+    print("stage 1 : changes added\n")
     print(one[1])
     prnt = input("Enter the commit text\n")
     two = subprocess.getstatusoutput(f'git commit -m \"{prnt}\"')
     time.sleep(20)
+    print("Stage 2 : changes commited\n")
     print(two[1])
     three = subprocess.getstatusoutput(f'git status')
     print(three[1])
