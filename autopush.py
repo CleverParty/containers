c8d6a87dea869d1b593d3c136251ca7514723e8e
CleverParty <shanatmail@gmail.com>
2020-07-22 17:58:12 -0400

    [update] auto 2.1

--- autopush.py
+++ autopush.py
@@ -4,15 +4,17 @@ s = subprocess.getstatusoutput(f'git status')
 if (s[0] == 0):
     one = subprocess.getstatusoutput(f'git add .')
     time.sleep(20)
+    print(one[1])
     prnt = input("Enter the commit text\n")
     two = subprocess.getstatusoutput(f'git commit -m \"{prnt}\"')
     time.sleep(20)
-    print(two)
+    print(two[1])
     three = subprocess.getstatusoutput(f'git status')
+    print(three[1])
     time.sleep(5)
 else :
     print("Commit something first thalaiva")
 
 # brnch = input("Enter the branch name\n")
 four = subprocess.getstatusoutput(f'git push') # test with origin later
-print(four)
+print(four[1])
