112adcc09f538f530af1d5db719e5ff1f0c55f5a
CleverParty <shanatmail@gmail.com>
2020-07-24 21:59:37 -0400

    [test] check flow 1.1

--- automations/autopush.py
+++ automations/autopush.py
@@ -8,12 +8,12 @@ tobestaged = "Changes not staged for commit:"
 def autoPush():
     if(tobestaged in s[1]) :
         one = subprocess.getstatusoutput(f'git add -A')
-        print("Stage 1 : changes added\n")
+        print("Stage 1 : Changes added\n")
         print(one[1])
         prnt = input("Enter the commit text\n")
         two = subprocess.getstatusoutput(f'git commit -m \"{prnt}\"')
         time.sleep(5) # if a larger commit, change sleep time
-        print("Stage 2 : changes commited\n")
+        print("Stage 2 : Changes commited\n")
         print(two[1])
         three = subprocess.getstatusoutput(f'git status')
         print(three[1])
@@ -24,9 +24,9 @@ def autoPush():
         print("Everything is up to date don't worry") """
 
 
-# brnch = input("Enter the branch name\n")
+brnch = input("Enter the branch name\n")
 brnch = "master"
 autoPush()
 four = subprocess.getstatusoutput(f'git push origin {brnch}') # test with origin later
-print("Stage 3 : git pushed")
+print("Stage 3 : git pushed changes")
 print(four[1])
