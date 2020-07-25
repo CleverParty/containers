a79caeb2ec7c4add341a444db6abf130b32ff39c
CleverParty <shanatmail@gmail.com>
2020-07-25 17:34:45 -0400

    [update] edits to workaround

--- nash/poker.py
+++ nash/poker.py
@@ -4,7 +4,14 @@ import nashpy as nashie
 seq = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
 
 def high_card(hand):
-    allfaces = [f for f,s in hand]
-    if allfaces[0] == 'A' :
+    faces = [f for f,s in hand]
+    if faces[0] == 'A' :
         print("Ace high")
-    return "high_card", sorted(allfaces, key=lambda f: allfaces.index(f), reverse=True)[0]
+    return "high_card", sorted(faces, key=lambda f: faces.index(f), reverse=True)[0]
+
+def decideHand(hand):
+    suit = [s for f,s in hand]
+    flush = suit.count(suit[0]) == len(suit) 
+    if(flush):
+        print("Flush baby")
+    return flush
