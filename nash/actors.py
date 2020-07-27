812c27ba02d796a17d4dd4eca74804e233a61262
CleverParty <shanatmail@gmail.com>
2020-07-26 22:15:02 -0400

    [test] small

--- nash/actors.py
+++ nash/actors.py
@@ -2,22 +2,23 @@ import nashpy as nash
 import numpy as np
 
 def rPS(flag):
-    rockPaperScissors= nash.Game(A)
-    sigma_r = [0, 0, 1]
-    sigma_c = [0, 1, 0]
-    print(rockPaperScissors[sigma_r, sigma_c])
-    # calculating nash equilibria 
-    # equilibrium = rockPaperScissors.support_enumeration()
-    print("\nThe nash equilibrium values represented with payoffs :\n")
-    # convert equillibrium to list type to print
-    # with random seed 
-    iterations = 100
-    np.random.seed(10)
-    if( flag == 'random' ):
-        count = rockPaperScissors.fictitious_play(iterations = iterations)
-        print("\nWith random seed generation for each actors (RPS) :\n")
-        for i, j in count:
-            print(i, j)
+    if( flag == 'normal' ):
+        rockPaperScissors= nash.Game(A)
+        sigma_r = [0, 0, 1]
+        sigma_c = [0, 1, 0]
+        print(rockPaperScissors[sigma_r, sigma_c])
+        # calculating nash equilibria 
+        # equilibrium = rockPaperScissors.support_enumeration()
+        print("\nThe nash equilibrium values represented with payoffs :\n")
+        # convert equillibrium to list type to print
+        # with random seed 
+        iterations = 100
+        np.random.seed(10)
+    elif( flag == 'random' ):
+            count = rockPaperScissors.fictitious_play(iterations = iterations)
+            print("\nWith random seed generation for each actors (RPS) :\n")
+            for i, j in count:
+                print(i, j)
     
 
 def matchingPennies():
