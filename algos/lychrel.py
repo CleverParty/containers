b60ab69fa94b9f19f6128d9bd9d97c1f056265a2
CleverParty <shanatmail@gmail.com>
2020-07-30 17:24:01 -0400

    [update] lychrel 1.1

--- algos/lychrel.py
+++ algos/lychrel.py
@@ -7,16 +7,15 @@ def palindrome(number):
     else:
         return False
 
-def lychrel(cargo):
+def lychrel(cargo , iter = 0):
     sumVal = int(str(cargo)[::-1])  +  cargo
+    print(sumVal)
+    GlobalCounter + 1
     if(palindrome(sumVal)):
-        print(f'this took {GlobalCounter+1} iterations')
+        print(f'this took {iter} iterations')
     else :
-        lychrel(sumVal)
-        print(f'this took {GlobalCounter+1} iterations')
+        lychrel(sumVal, iter+1 ) 
 
-lychrel(123)
-
-
-
-print(palindrome(125372))
+lychrel(89)
+# for n in range(0,100):
+#     print(lychrel(n))
