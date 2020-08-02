024aac67ab29884dea455e7c5060a4de867d74a7
CleverParty <shanatmail@gmail.com>
2020-08-02 02:23:17 -0400

    [update] changes

--- algos/squareConvergents.py
+++ algos/squareConvergents.py
@@ -2,12 +2,15 @@ import sys
 from fractions import Fraction
 sys.setrecursionlimit(10000)
 a = 2
-root = 2**2
+root = a**2
 def convergent(num):
     base = 1 
     if num == 0:
-        return True
-    sumCon = base +1/(1/2 + convergent(num-1)) # this just calculates the convergence recursively, for full solution it would be better to find numerator and denominator seperatelty in a for/while loop
+        return False
+    elif num > 1000 :
+        return None
+    num -= 1
+    sumCon = base +1/(1/2 + convergent(num)) # this just calculates the convergence recursively, for full solution it would be better to find numerator and denominator seperatelty in a for/while loop
     return sumCon
 
 print(convergent(1000))
