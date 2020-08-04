d055e2a8bfbdd11c2f8c4f6c5cedfd0d12e80a2c
CleverParty <shanatmail@gmail.com>
2020-08-04 09:16:32 -0400

    [update] gerating probabilities

--- algos/stPetersburgLottery.py
+++ algos/stPetersburgLottery.py
@@ -1,5 +1,8 @@
 import os,math
 import numpy as np
+import random as r 
+from random import shuffle
+
 x = np.array([0.23,0.5,0.8])
 
 def Step(item):
@@ -9,5 +12,15 @@ for i in x:
     rtrnval = Step(i)
     print(rtrnval)
 
+# shuffle the possible H/T
+
+def generateOutcomes():
+    for i in range(100):
+        h = r.uniform(0, 0.5)
+        t = 1 - h
+    return h,t
+
+
 stepfunc = np.heaviside(x, 0.5)
 print(stepfunc)
+generateOutcomes()
