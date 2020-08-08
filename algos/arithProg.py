19d6bdccc30913b7b0e326a493cc69b0a8081e8f
CleverParty <shanatmail@gmail.com>
2020-08-08 17:44:14 -0400

    [update] edits 1.3

--- algos/arithProg.py
+++ algos/arithProg.py
@@ -3,15 +3,16 @@ from datetime import datetime as time
 inA = [0, 4, 8, 16]
 inB = [0, 2, 6, 12, 14, 20] 
 apList = []
-def arith(a,n,d):
+def arith(arr,a,n,d):
     base = a
     if(d==None):
         d = 0
     sumn = (n/2) * (2*base + (n-1)*d)
-    for i in range(a,n):
+    for i in range(0,len(arr)):
+        base = arr[i]
         sn = base + (i-1)*d
         snp1 = base + (i)*d
-        print(f'{i} and + 1 = {snp1}')
+        print(f"{base} and + 1 = {snp1}")
         apList.append(sn)
     print(apList)
     return(apList,sumn)
@@ -23,10 +24,10 @@ def arith(a,n,d):
 
 def main():
     begin = time.now()
-    rtnList,sumn = arith(1,10,3)
+    # rtnList,sumn = arith(merged,1,10,3)
     merged = inA + inB
     merged.sort()
-    rtnList, sumn = arith(merged[0],merged[-1],None)
+    rtnList, sumn = arith(merged,merged[0],merged[-1],None)
     # print(f'merged = {merged} , max = {maximum}')
     print(f'the list is {rtnList}, sum = {sumn}')
     # print(arith(10290,989899989899595354548798000000000000000000000000000000000000006547655555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555000000000000006876878766666666667987657435675899567908976858789897679876890876890898768756565374754756466745896079875676574675865745765867465876857686797807967856746576780987654536789765456789765435678976546778674697856475768767567687086958687086758676870656476567964345674334766354365476586745500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000023423423423413515167834523526534535000000,12312423445))
