1df66601bf00120cca878d025d50f27b5e3efac2
CleverParty <shanatmail@gmail.com>
2020-08-08 20:52:31 -0400

    [update] edits 1.4

--- algos/arithProg.py
+++ algos/arithProg.py
@@ -5,6 +5,7 @@ inB = [0, 2, 6, 12, 14, 20]
 apList = []
 def arith(arr,a,n,d):
     base = a 
+    temp = 0
     if(d==None):
         d = 0
     sumn = (n/2) * (2*base + (n-1)*d)
@@ -12,13 +13,16 @@ def arith(arr,a,n,d):
         base = arr[i]
         print(base)
         sn = base + (i-1)*d
-        snp1 = base + (i)*d
+        snp1 = base + (i-2)*d
+        temp = d
+        print("snp1 is ",snp1)
         if(snp1 not in arr):
-            d=d+1
-        print(f'{base} and + 1 = {snp1}')
-        apList.append(sn)
+            continue
+        else:
+            print(f'{sn} and + 1 = {snp1}')
+            apList.append(sn)    
     print(apList)
-    return(apList,sumn)
+    return(apList,temp)
 
 
 """ def process(arr): # should be sorted before passing it here
@@ -31,9 +35,17 @@ def main():
     merged = inA + inB
     merged.sort()
     # merged[-1:] ==> last index (len-1)
-    rtnList, sumn = arith(merged,merged[0],merged[-1],None)
+    d = 0
+
+    for i in range(0,len(merged)-1):
+        s = merged[i] + (i-1)*d 
+        if(s not in merged):
+            d += 1
+            print(s)
+
+    # rtnList, sumn = arith(merged,merged[0],merged[-1],None)
     # print(f'merged = {merged} , max = {maximum}')
-    print(f'the list is {rtnList}, sum = {sumn}')
+    # print(f'the list is {rtnList}, sum = {sumn}')
     # print(arith(10290,989899989899595354548798000000000000000000000000000000000000006547655555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555000000000000006876878766666666667987657435675899567908976858789897679876890876890898768756565374754756466745896079875676574675865745765867465876857686797807967856746576780987654536789765456789765435678976546778674697856475768767567687086958687086758676870656476567964345674334766354365476586745500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000023423423423413515167834523526534535000000,12312423445))
     end = time.now()
     print(f"elapsed = {end - begin}")
