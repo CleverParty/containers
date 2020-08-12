d294436c3a58d4aa67163d78ee3bc727844cdb51
CleverParty <shanatmail@gmail.com>
2020-08-12 12:15:54 -0400

    [update] changes 1.2

--- algos/kxkMatrices.py
+++ algos/kxkMatrices.py
@@ -8,6 +8,8 @@ def matrix(arr,k):
     print(res[0][2])
     for i in range(0,len(arr[0])):
         for j in range(i, k):
+            if((i+j) == (i+1)):
+                print("logix")
             if(j == (i+1)):
                 print(f'{i} and {j}')
                 print(res[i][j])
