d46ec104113cd91c6d4907c1e0186903a460bd3f
CleverParty <shanatmail@gmail.com>
2020-08-12 12:00:08 -0400

    [update] changes 1.1

--- algos/kxkMatrices.py
+++ algos/kxkMatrices.py
@@ -3,19 +3,19 @@ import subprocess
 GlobalConst = 0
 GlobalList = [[2, 4, 6], [3, 6, 9], [4, 8, 12]] 
 
-def matrix(arr):
+def matrix(arr,k):
     res = [c for c in arr]
     print(res[0][2])
     for i in range(0,len(arr[0])):
-        for j in range(0, len(arr[0])):
+        for j in range(i, k):
             if(j == (i+1)):
                 print(f'{i} and {j}')
-            print(res[i][j])
+                print(res[i][j])
     # sub = [c,x for x in arr]
 
 
 def main():
-    matrix(GlobalList)
+    matrix(GlobalList,2)
 
 if __name__ == "__main__":
     main()
