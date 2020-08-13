4e190442e05fad00590e34ea3c8fd48ada08c2bf
CleverParty <shanatmail@gmail.com>
2020-08-13 11:17:10 -0400

    [test] updates 1.2

--- algos/kxkMatrices.py
+++ algos/kxkMatrices.py
@@ -1,14 +1,14 @@
 import math,os
 import subprocess
 GlobalConst = 0
-GlobalList = [[2, 4, 6], [3, 6, 9], [4, 8, 12]] 
+GlobalList = [[2, 4, 6], [3, 6, 9], [4, 8, 12]]
 
 def matrix(arr,k):
     res = [c for c in arr]
     print(res[0][2])
     for i in range(0,len(arr[0])):
         for j in range(i, k):
-            if((i+j) == (i+1) and j == (i+1)):
+            if((i+j) == (i+1) or j == (i+1)):
                 print("logix")
             if(j == (i+1)):
                 print(f'{i} and {j}')
