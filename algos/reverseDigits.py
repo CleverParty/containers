7fc7658aad4b0b7ac48ad2933bc5e4c771061683
CleverParty <shanatmail@gmail.com>
2020-08-15 11:42:51 -0400

    [update] changes 1.1

--- algos/reverseDigits.py
+++ algos/reverseDigits.py
@@ -1,23 +1,25 @@
 import os,math
 
 numList = []
+GlobalStore = 0
 
 def checkList(arr):
     length = len(str(arr))
     if(length%2 != 0):
         flag = True
+        GlobalStore = length**2
         return(flag,length)
     else:
         res = [x for x in arr]
-        print(res)
         flag = False
         return(flag,length)
 
 
 def main():
-    arr = [12341345]
+    arr = [12341345,234158292,8002842985]
     flag,length = checkList(arr)
     print(flag,length)
+    print(GlobalStore)
 
 
 if __name__ == "__main__":
