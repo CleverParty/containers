cbaee441154aef2394d1266937d47d5a00f2adda
CleverParty <shanatmail@gmail.com>
2020-08-23 12:50:41 -0400

    [update] edits 1.1

--- data/tickerScanner.py
+++ data/tickerScanner.py
@@ -20,10 +20,28 @@ with open("access.txt","r") as access:
             if(cnt>2):
                 break
 
+# limit 30/sec base
 r = requests.get('https://finnhub.io/api/v1/company-news?symbol=TSLA&from=2020-04-30&to=2020-05-01&token=',stripped)
 print(r.json())
 print(stripped)
 accessfile.close()
+
+def genWebHook():
+    r = requests.post('https://finnhub.io/api/v1/webhook/add?token=', stripped , json={'event': 'earnings', 'symbol': 'TSLA'})
+    res = r.json()
+    print(res)
+    webhook_id = res['id']
+    # List webhook
+    r = requests.get('https://finnhub.io/api/v1/webhook/list?token=bt0rj6748v6ptb8skvpg')
+    res = r.json()
+    print(res)
+
+def main():
+    genWebHook()
+
+if __name__ == "__main__":
+    main()
+
 # quandl.ApiConfig.api_key = contents
 # data = quandl.get('WIKI/TSLA', start_date='2019-12-26', end_date='2020-7-28')
 
