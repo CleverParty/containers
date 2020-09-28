7f5ccee25487b0596a32f1956507ebb6ab925ff5
CleverParty <shanatmail@gmail.com>
2020-09-27 23:31:15 -0400

    [update] changes

--- data/tickerPackageStaging.py
+++ data/tickerPackageStaging.py
@@ -9,6 +9,18 @@ def createPinkfishSymbol(symbol):
     tsData = pf.fetch_timeseries(symbol)
     print(tsData.tail())
 
+class merkle(self):
+    def __init__(self,root,originalChain):
+        self.root = root
+        self.completeTransactions = completeTransactions
+        self.prevTransactions = prevTransactions
+        self.currentHash = currentHash
+    
+    def create(self):
+        self.completeTransactions = self.prevTransactions
+        self.currentHash = hashlib.sha256(currentTransaction)
+        self.doubleHash = hashlib.sha256(self.currentHash)
+    
 def main():
     start = datetime.datetime(2020,9,12) # format :- year,month,day
     end = datetime.datetime(2020,9,16)
