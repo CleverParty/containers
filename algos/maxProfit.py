69b2279d096582db9655c2d9de52047558cb314a
CleverParty <shanatmail@gmail.com>
2020-09-28 22:11:29 -0400

    [update] changes

--- algos/maxProfit.py
+++ algos/maxProfit.py
@@ -1,20 +1,35 @@
-prices = [3,3,5,0,0,3,1,4]
+prices = [7,6,4,3,1]
+defaultStartAvg = sum(prices)/len(prices)
+print(defaultStartAvg)
 
-def avgUpdater(index,prices):
-    avg = prices[index] + prices[index] + prices[index - 1]
+def avgUpdater(index,prices,period):
+    return((prices[index] + prices[index] + prices[index - 1])/period)
 
 def sma(prices,period):
-    defaultStartAvg = sum(prices)/len(prices)
-    print(defaultStartAvg)
     movingAvg = []
     for i in range(0,len(prices)-2):
         avg  = (prices[i] + prices[i+1] + prices[i+2])/period
         movingAvg.append(avg)
     print(movingAvg)
     return movingAvg
-        
+
+movingAverages = sma(prices=prices,period=3)
+print(movingAverages)
+def buySell(prices,period):
+    signals = []
+    for i in range(0,len(prices)):
+        if( i<=period and prices[i] < defaultStartAvg):
+            signals.append("B")
+            print(f'with price:{prices[i]}, we are to "B"')
+        elif( i>period and prices[i] > movingAverages[i]):
+            signals.append("S")
+            print(f'with price:{prices[i]}, we are to "S"')
+        else:
+            signals.append("S")
+        print(signals)
+    return signals
+
+
 
 # EMA = (K * ( C - P )) + P 
-sma(prices=prices,period=3)
-print((prices[0]+prices[1]+prices[2])/3)
-
+buySell(prices=prices,period=3)
