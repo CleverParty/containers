dca53e7d56ad2dba2fdbc1b95be9ddbb72de1c37
CleverParty <shanatmail@gmail.com>
2020-12-25 20:34:24 -0500

    [update] lorenz equations

--- data/testChaosTheoryBase.py
+++ data/testChaosTheoryBase.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D 
 
 # Global consts for fluid flow (included in Lorenz equations)
@@ -7,16 +8,18 @@ prandtl_number = 10 # AKA. sigma in certain texts, ratio of momentum diffusivity
 rayleigh_number = 28 # it is the measure of instability in a fluid, mainly caused by convection
 beta = 2.667 # abs(8/3) measure of compressibility of a fluid ( relative volume change )
 # refer "https://mathworld.wolfram.com/LorenzAttractor.html" for complete formulaic breakdown
-def lorenzAttractorGenerator(x,y,z):
+def lorenzAttractorGenerator(X, t):
     # This method of fluid movement, essentially assumes that the fluid is cooled from below and heated from above
-    xBar = prandtl_number * (y-x)
+    x, y, z = X
+    xBar = -prandtl_number * (y - x)
     yBar = rayleigh_number * x - y - x*y
-    zBar = x * y - beta * z
+    zBar = -beta*z + x*y 
     return xBar, yBar, zBar
 
 if __name__ == "__main__":
-    dt = 0.01
-    steps = 10000
+    dt = 100 # max time point
+    steps = 10000 # total no of time points
+    """ 
     xArr = np.empty(steps + 1)
     yArr = np.empty(steps + 1)
     zArr = np.empty(steps + 1)
@@ -26,15 +29,27 @@ if __name__ == "__main__":
         xArr[i + 1] = xArr[i] + (xBar * dt)
         yArr[i + 1] = yArr[i] + (yBar * dt)
         zArr[i + 1] = zArr[i] + (zBar * dt)
+    """
+    t = np.linspace(0, dt, steps)
+    f = odeint(lorenzAttractorGenerator, (0, 1, 1.05), t)
+    x, y, z = f.T
+    print(x,y,z)
     
+    """
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    s = 10
+    c = np.linspace(0,1,n)
+    for i in range(0,n-s,s):
+    ax.plot(x[i:i+s+1], y[i:i+s+1], z[i:i+s+1], color=(1,c[i],0), alpha=0.4)
     ax.plot(xArr, yArr, zArr, lw=0.5)
     ax.set_xlabel("X Axis")
     ax.set_ylabel("Y Axis")
     ax.set_zlabel("Z Axis")
     ax.set_title("Lorenz Attractor")
     plt.show()
+    """
+
     pass
 
 
